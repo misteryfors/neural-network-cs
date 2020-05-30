bc77f174841acb1db1fdf0d004a979c29c182b10
bonmas14 <bonmas14@gmail.com>
2020-05-30 14:50:24 +0400

    new version of net 2.0 50% end

---
+++ NerualNetwork_2.0/NetConstructor.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerualNetwork_2._0
+{
+    public class NetConstructor
+    {
+        public NeuronTypes[][] NetTypes
+        {
+            get;
+            private set;
+        }
+
+        public NetConstructor(int[][] netMaket)
+        {
+            NetTypes = new NeuronTypes[netMaket.Length][];
+
+            for (int i = 0; i < netMaket.Length; i++)
+            {
+                if (i != netMaket.Length - 1)
+                    NetTypes[i] = new NeuronTypes[netMaket[i].Length + 1];
+                else
+                    NetTypes[i] = new NeuronTypes[netMaket[i].Length];
+            }
+            for (int i = 0; i < NetTypes.Length; i++)
+            {
+                if (i == 0)
+                {
+                    for (int j = 0; j < NetTypes[i].Length; j++)
+                    {
+                        if (j == NetTypes[i].Length - 1)
+                        {
+                            NetTypes[i][j] = NeuronTypes.BIAS;
+                            continue;
+                        }
+                        else
+                        {
+                            NetTypes[i][j] = NeuronTypes.INPUT;
+                        }
+                    }
+                    continue;
+                }
+                if (i == NetTypes.Length - 1)
+                {
+                    for (int j = 0; j < NetTypes[i].Length; j++)
+                    {
+
+                        NetTypes[i][j] = NeuronTypes.OUTPUT;
+                    
+                    }
+                    continue;
+                }
+
+                for (int j = 0; j < NetTypes[i].Length; j++)
+                {
+                    if (j == NetTypes[i].Length - 1)
+                    {
+                        NetTypes[i][j] = NeuronTypes.BIAS;
+                        continue;
+                    }
+                    else
+                    {
+                        NetTypes[i][j] = NeuronTypes.HIDDEN;
+                    }
+                }
+            }
+        }
+    }
+}
--- NerualNetwork_2.0/Neuron.cs
+++ NerualNetwork_2.0/Neuron.cs
@@ -1,12 +1,149 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace NerualNetwork_2._0
 {
+    public enum NeuronTypes
+    {
+        INPUT,
+        HIDDEN,
+        BIAS,
+        OUTPUT
+    }
     class Neuron
     {
+        public List<Neuron> prewLayer;
+
+        public double[] Weigts
+        {
+            get;
+            private set;
+        }
+
+        public NeuronTypes Type
+        {
+            get;
+            private set;
+        }
+
+        public double OutputData
+        {
+            get;
+            private set;
+        }
+
+        public double Error
+        {
+            get;
+            private set;
+        }
+
+        int indexInLayer;
+
+        public Neuron(NeuronTypes type, NeuronTypes[] prewLayerTypes, List<Neuron> _prewLayer, int _indexInLayer, double stdWeight = 0.1)
+        {
+            indexInLayer = _indexInLayer;
+
+            Type = type;
+
+            prewLayer = _prewLayer;
+            
+            int weigtsCount = 0;
+
+            for (int i = 0; i < prewLayerTypes.Length; i++)
+            {
+                if (type == NeuronTypes.INPUT || type == NeuronTypes.BIAS)
+                {
+                    if (type == NeuronTypes.BIAS)
+                    {
+                        OutputData = 1;
+                    }
+                    return;
+                }
+
+                if (prewLayerTypes[i] != NeuronTypes.BIAS)
+                {
+                    weigtsCount++;
+                }
+            }
+
+            Weigts = new double[weigtsCount];
+            for (int i = 0; i < Weigts.Length; i++)
+            {
+                Weigts[i] = stdWeight;
+            }
+        }
+
+        double ActivationFunc(double data)
+        {
+            return 1 / (1 + Math.Pow(Math.E, -data));
+        }
+
+        public bool DataUpdate()
+        {
+            if (Type == NeuronTypes.INPUT || Type == NeuronTypes.BIAS)
+                return false;
+
+            double[] neuron = new double[prewLayer.Count];
+            for (int i = 0; i < prewLayer.Count; i++)
+            {
+                neuron[i] = prewLayer[i].OutputData * Weigts[i];
+            }
+
+            double data = 0;
+            for (int i = 0; i < neuron.Length; i++)
+            {
+                data += neuron[i];
+            }
+
+            OutputData = ActivationFunc(data);
+            return true;
+        }
+        public bool DataUpdate(double data)
+        {
+            if (Type != NeuronTypes.INPUT)
+                return false;
+
+            OutputData = ActivationFunc(data);
+            return true;
+        }
+        public void Correct(double learnSpeed)
+        {
+            if (Type == NeuronTypes.INPUT || Type == NeuronTypes.BIAS)
+                return;
+
+            double deltaError = Error * (OutputData * (1 - OutputData));
+
+            for (int i = 0; i < prewLayer.Count; i++)
+            {
+                double correct = deltaError * prewLayer[i].OutputData * learnSpeed;
+                Weigts[i] += correct;
+            }
+        }
+
+        public void FindError(List<Neuron> NextLayer, double needOut = 0)
+        {
+            Error = 0;
+
+            if (Type == NeuronTypes.INPUT || Type == NeuronTypes.BIAS)
+                return;
+
+            if (Type == NeuronTypes.OUTPUT)
+            {
+                Error = needOut - OutputData;
+                return;
+            }
+            else
+            {
+                for (int i = 0; i < NextLayer.Count; i++)
+                {
+                    Error += prewLayer[i].Weigts[indexInLayer] * NextLayer[i].OutputData;
+                }
+            }
+        }
     }
 }
